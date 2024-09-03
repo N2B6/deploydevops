5268295668a10d95cf11cbd464c4acfc95d82460
N2B6 <nipun.bakshi262001@gmail.com>
2024-09-03 01:09:52 +0100

    final update

--- ProjectTravel/urls.py
+++ ProjectTravel/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 
@@ -24,3 +25,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Wanderlust.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
